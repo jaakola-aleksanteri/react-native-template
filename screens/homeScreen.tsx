213fd07d045d4c268251962684b5aa82ddb744bb
Aleksanteri Jaakola <aleksanteri.jaakola@twoday.com>
2023-09-14 20:55:56 +0300

    removed unused import

--- screens/homeScreen.tsx
+++ screens/homeScreen.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { View, Text } from 'react-native';
 import { useTheme } from '@react-navigation/native';
-import styled from 'styled-components/native';
 
 const HomeScreen = (): React.JSX.Element => {
   const { colors } = useTheme();
